29cb263670ac1c04c64fd7cc532659a11a6773d1
Ryan Wenger <ryanwenger@umail.ucsb.edu>
2022-02-06 17:37:29 -0800

    rw - implemented POST method for /api/UCSBSubjects/post endpoint to add subjects to the table

--- src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectController.java
@@ -1,7 +1,6 @@
 package edu.ucsb.cs156.team02.controllers;
 
 import edu.ucsb.cs156.team02.entities.UCSBSubject;
-import edu.ucsb.cs156.team02.entities.User;
 import edu.ucsb.cs156.team02.models.CurrentUser;
 import edu.ucsb.cs156.team02.repositories.UCSBSubjectRepository;
 import io.swagger.annotations.Api;
@@ -9,23 +8,16 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.Optional;
 
 @Api(description = "UCSBSubjects")
 @RequestMapping("/api/UCSBSubjects")
@@ -56,9 +48,37 @@ public class UCSBSubjectController extends ApiController {
     ObjectMapper mapper;
 
     @ApiOperation(value = "List all subjects")
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<UCSBSubject> allSubjects() {
         loggingService.logMethod();
         return subjectRepository.findAll();
+    }
+
+    @ApiOperation(value = "Create a new UCSBSubject")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PostMapping("/post")
+    public UCSBSubject postSubject(
+            @ApiParam("subject code") @RequestParam String subjectCode,
+            @ApiParam("subject translation") @RequestParam String subjectTranslation,
+            @ApiParam("department code") @RequestParam String deptCode,
+            @ApiParam("college code") @RequestParam String collegeCode,
+            @ApiParam("related department code") @RequestParam String relatedDeptCode,
+            @ApiParam("inactive") @RequestParam Boolean inactive,
+            @ApiParam("done") @RequestParam Boolean done) {
+        loggingService.logMethod();
+        CurrentUser currentUser = getCurrentUser();
+
+        UCSBSubject subject = new UCSBSubject();
+        subject.setUser(currentUser.getUser());
+        subject.setSubjectCode(subjectCode);
+        subject.setSubjectTranslation(subjectTranslation);
+        subject.setDeptCode(deptCode);
+        subject.setCollegeCode(collegeCode);
+        subject.setRelatedDeptCode(relatedDeptCode);
+        subject.setInactive(inactive);
+        subject.setDone(done);
+
+        return subjectRepository.save(subject);
     }
 }
